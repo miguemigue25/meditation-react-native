0093562aac3aa5a524ddea02aa44f2e0e9b24c5d
Miguel <migueortiz0125@gmail.com>
2024-07-22 19:00:31 -0700

    meditation detail screen completed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,22 @@
-import { Slot, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { Slot, SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+    const [fontsLoaded, error] = useFonts({
+        "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+    });
+
+    useEffect(() => {
+        if (error) throw error;
+        if (fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded, error]);
+
+    if (!fontsLoaded) return null;
+    if (!fontsLoaded && !error) return null;
+
     return (
         <Stack>
             <Stack.Screen
@@ -9,6 +25,10 @@ export default function RootLayout() {
             />
             <Stack.Screen
                 name="index"
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="meditate/[id]"
                 options={{ headerShown: false }}
             />
         </Stack>
